b276212c8fa8904d487df041111f74c74110f1ff
limsungju <fullmoon4860@gmail.com>
2019-09-17 09:54:59 +0900

    ActionFactory로 서블릿 정형화

--- src/main/java/kr/co/itcen/mysite/controller/MainServlet.java
+++ src/main/java/kr/co/itcen/mysite/controller/MainServlet.java
@@ -1,18 +1,29 @@
 package kr.co.itcen.mysite.controller;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import kr.co.itcen.web.WebUtils;
+import kr.co.itcen.mysite.action.main.MainActionFactory;
+import kr.co.itcen.web.mvc.Action;
+import kr.co.itcen.web.mvc.ActionFactory;
 
 public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		WebUtils.forward(request, response, "/WEB-INF/views/main/index.jsp");
+		
+		request.setCharacterEncoding("utf-8");
+		
+		String actionName = request.getParameter("a");
+		
+		//new MainActionFactory().getAction(actionName).execute
+		ActionFactory actionFactory = new MainActionFactory();
+		Action action = actionFactory.getAction(actionName);
+		action.execute(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
