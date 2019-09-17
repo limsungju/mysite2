d32d17d8a6ce6bd8093ab310235512c5841c97a9
limsungju <fullmoon4860@gmail.com>
2019-09-17 10:49:54 +0900

    MainServlet

--- src/main/java/kr/co/itcen/mysite/controller/MainServlet.java
+++ src/main/java/kr/co/itcen/mysite/controller/MainServlet.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import kr.co.itcen.mysite.action.main.MainActionFactory;
+import kr.co.itcen.mysite.action.user.UserActionFactory;
 import kr.co.itcen.web.mvc.Action;
 import kr.co.itcen.web.mvc.ActionFactory;
 
@@ -15,13 +15,12 @@ public class MainServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
 		request.setCharacterEncoding("utf-8");
 		
 		String actionName = request.getParameter("a");
 		
 		//new MainActionFactory().getAction(actionName).execute
-		ActionFactory actionFactory = new MainActionFactory();
+		ActionFactory actionFactory = new UserActionFactory();
 		Action action = actionFactory.getAction(actionName);
 		action.execute(request, response);
 	}
